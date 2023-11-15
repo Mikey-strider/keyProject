808756c1d963bbf4459858152d686f261d44db36
Mikey Strider <dromen2001@gmail.come.com>
2023-11-15 10:21:38 -0800

    Beginning of key project

---
+++ keyLock.js
@@ -0,0 +1,32 @@
+function modifyContent(){
+
+const keyBuild = document.querySelector(".key-locker");
+const dayCalendar = "";
+const keyArray = ["keyZero", "key1", "key2"];
+const keyGen = "";
+const keyZero = "Lock up keys!";
+// const keyClaim = "";
+const keyDiscount = "";
+
+const claimForm = "";
+const returnClaimForm = "";
+
+keyBuild.innerHTML = `
+  <div class="wrapper">
+    <div class="claim-form">Yay I work!</div>
+  </div>
+  
+`
+
+function keyClaim(keyArray){
+  if(!keyZero){
+    const keyView = keyArray.splice(2, 1)[0];
+    console.log(keyView);
+  } else {
+    return keyZero;
+  }
+}
+
+keyClaim();
+}
+window.addEventListener('DOMContentLoaded', modifyContent);
